f0cd0ae31ddc428477f8a8cb682dce2844ce7162
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-03-31 00:04:49 +0200

    ...

--- api/src/config/dotenv.ts
+++
@@ -1,5 +0,0 @@
-export default{
-    port: 1999,
-    MongoDB: "mongodb+srv://mohamedmostafa:09705886877a@blogs.cq7zo.mongodb.net/DARK?retryWrites=true&w=majority",
-    URL: "http://localhost:1999"
-}
--- api/src/model/User.ts
+++ api/src/model/User.ts
@@ -1,5 +1,25 @@
 import { Schema, model } from 'mongoose';
 
 const newUser = new Schema({
-    
-})
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 500
+    },
+    ProfileImage: {
+        type: String,
+        default: ""
+    }
+}, {timestamps: true})
+
+const user = model('User', newUser);
+
+export default user;
