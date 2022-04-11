2c898c524e92236a79a25cf3f887ffc4810093fb
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-04-12 00:35:39 +0200

    .

--- api/src/config/dotenv.ts
+++ api/src/config/dotenv.ts
@@ -1,5 +1,5 @@
 export default {
     PART: 1999,
     HostURL: 'http://localhost:1999',
-    mongoDB: 'mongodb+srv://mohamedmostafa:09705886877a@aroowsa7lw.qy2ok.mongodb.net/Anime?authSource=admin&replicaSet=atlas-onxxax-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true'
+    mongoDB: 'XXXX'
 }
